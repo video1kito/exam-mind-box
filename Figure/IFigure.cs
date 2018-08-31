41b78b09bf67e3af788b9ba0114beaf2d3e956a3
Кита Китов <video.1kito@gmail.com>
2018-08-31 14:45:35 +0300

    Remove extra project

--- Figure/IFigure.cs
+++ Figure/IFigure.cs
@@ -3,17 +3,10 @@
     public interface IFigure
     {
         /// <summary>
-        /// Calculation the areas for currnet figure
+        /// Calculation the area for figure
         /// </summary>
         /// <param name="parts"></param>
         /// <returns></returns>
         double Area(params double[] parts);
-
-        /// <summary>
-        /// Calculation the areas for any figures
-        /// </summary>
-        /// <param name="parts"></param>
-        /// <returns></returns>
-        // double AreaForUnknown(params double[] parts);
     }
 }
